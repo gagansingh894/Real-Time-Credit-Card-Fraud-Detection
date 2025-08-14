0497f3c64ec2b35dd7cb455b4069672da5093819
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2025-08-14 19:21:56 +0100

    feat: setup etl config, schema and spark session

---
+++ spark_jobs/etl/session.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+
+from spark_jobs.etl.config import *
+
+def get_spark_session(app_name: str) -> SparkSession:
+    return (
+        SparkSession.builder
+        .appName(app_name)
+        .config("spark.cassandra.connection.host", CASSANDRA_HOST)
+        .config("spark.cassandra.connection.port", CASSANDRA_PORT)
+        .config("spark.jars.packages", "com.datastax.spark:spark-cassandra-connector_2.12:3.5.0")
+        .getOrCreate()
+    )
