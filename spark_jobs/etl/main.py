f720d7453c94e09a402c9c0b5043d5f9d402e420
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2025-08-14 19:22:10 +0100

    feat: etl job

---
+++ spark_jobs/etl/main.py
@@ -0,0 +1,29 @@
+import logging
+from spark_jobs.etl import config
+from spark_jobs.etl.ingest import load_customer_data, load_transaction_data, transform_transaction_dates, \
+    calculate_customer_age, process_data, write_to_cassandra
+from spark_jobs.etl.session import get_spark_session
+
+logger = logging.getLogger(__name__)
+
+def ingest_data_job():
+    # create spark session
+    spark = get_spark_session("cassandra_data_ingest")
+
+    # load data from file system
+    customer_df = load_customer_data(spark, config.CUSTOMER_DATA_PATH)
+    transaction_df = load_transaction_data(spark, config.TRANSACTION_DATA_PATH)
+
+    # process data
+    customer_df = calculate_customer_age(customer_df)
+    transaction_df = transform_transaction_dates(transaction_df)
+    fraud_df, non_fraud_df = process_data(customer_df, transaction_df)
+
+    # write data to cassandra
+    write_to_cassandra(customer_df, fraud_df, non_fraud_df)
+
+
+if __name__ == "__main__":
+    logger.info('Starting ingest job')
+    ingest_data_job()
+    logger.info('Finished ingest job')
