ee5768a7ae65d9d316e22cfa7ac9cd3797882660
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2025-08-20 00:11:49 +0100

    fix: mlflow setup

--- spark_jobs/utils/config.py
+++ spark_jobs/utils/config.py
@@ -12,5 +12,5 @@ CASSANDRA_NON_FRAUD_TRANSACTIONS_TABLE = 'non_fraud_transactions'
 PREPROCESSOR_PATH = 'spark_jobs/ml_training/artefacts/preprocessing'
 RANDOM_FOREST_MODEL_PATH = 'spark_jobs/ml_training/artefacts/randomforest'
 
-MLFLOW_TRACKING_URI = 'http://0.0.0.0:5000'
+MLFLOW_TRACKING_URI = 'http://0.0.0.0:5005'
 MLFLOW_EXPERIMENT_NAME = 'fraud-detection'
