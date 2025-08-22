d699c49d1490ef0380760d1ebaacef80dbaf41ab
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2025-08-22 22:23:37 +0100

    feat: revert to using two separate models

--- spark_jobs/ml_training/main.py
+++ spark_jobs/ml_training/main.py
@@ -12,13 +12,13 @@ def train_model_job():
     spark = get_spark_session("random_forest_model_training")
 
     # preprocessing pipeline
-    preprocessed_df, preprocessor = preprocessing_pipeline(spark, CASSANDRA_KEYSPACE)
+    preprocessed_df, preprocessor_pipeline_model = preprocessing_pipeline(spark, CASSANDRA_KEYSPACE)
 
     # balance dataframe since the ratio of fraudulent transactions is small compared to valid transactions
     train_df = balance_features_dataframe(preprocessed_df)
 
-    # train model - we pass preprocessor to create a single pipeline for transformation and prediction
-    trained_model = train_model(train_df, preprocessor)
+    # train model
+    trained_model = train_model(train_df)
 
     # persist to mlflow
     persist_artefacts(preprocessor_pipeline_model, trained_model)
@@ -27,5 +27,3 @@ if __name__ == "__main__":
     logger.info("Starting ML training job")
     train_model_job()
     logger.info("ML training job completed")
-
-
