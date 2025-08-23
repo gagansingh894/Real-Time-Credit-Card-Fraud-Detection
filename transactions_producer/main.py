8d508f20c51e47400723a4382a5137f0d464c4a2
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2025-08-23 01:51:19 +0100

    feat: update producer

--- transactions_producer/main.py
+++ transactions_producer/main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     transactions_df = pd.read_csv("transactions_producer/transactions.csv")
     transactions = transactions_df.to_dict(orient="records")
 
-    producer = TransactionsProducer(["0.0.0.0:9092", "0.0.0.0:9094", "0.0.0.0:9096"], topic="transactions")
+    producer = TransactionsProducer(["0.0.0.0:9092"], topic="transactions")
 
     for transaction in transactions:
         producer.send_transactions(transaction)
