9a6b6531f944d2ad9c9869070d4dfe14a6fd5c2a
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2025-08-23 03:25:15 +0100

    fix: spark session

--- spark_jobs/utils/session.py
+++ spark_jobs/utils/session.py
@@ -8,6 +8,7 @@ def get_spark_session(app_name: str) -> SparkSession:
         .appName(app_name)
         .config("spark.cassandra.connection.host", CASSANDRA_HOST)
         .config("spark.cassandra.connection.port", CASSANDRA_PORT)
-        .config("spark.jars.packages", "com.datastax.spark:spark-cassandra-connector_2.12:3.5.0")
+        .config("spark.jars.packages", "com.datastax.spark:spark-cassandra-connector_2.12:3.5.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0")
+
         .getOrCreate()
     )
