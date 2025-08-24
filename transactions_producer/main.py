d5ef539d5c439dcaf91b402c014af312ac213dec
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2025-08-24 02:46:15 +0100

    fix: kafka runner

--- transactions_producer/main.py
+++ transactions_producer/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 from transactions_producer.producer import TransactionsProducer
 
-if __name__ == "__main__":
+def run():
     transactions_df = pd.read_csv("transactions_producer/transactions.csv")
     transactions = transactions_df.to_dict(orient="records")
 
