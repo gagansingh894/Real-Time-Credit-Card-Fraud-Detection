eeb04fd5d8850a9542bdefab75bb1989a126dd98
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2025-08-25 20:06:19 +0100

    fix: mlflow path

--- spark_jobs/utils/config.py
+++ spark_jobs/utils/config.py
@@ -1,8 +1,8 @@
 CASSANDRA_HOST = 'cassandra1, cassandra2'
 CASSANDRA_PORT = 9042
 
-CUSTOMER_DATA_PATH = './spark_jobs/data/customer.csv'
-TRANSACTION_DATA_PATH = './spark_jobs/data/transactions.csv'
+CUSTOMER_DATA_PATH = "/opt/shared/data/customer.csv"
+TRANSACTION_DATA_PATH = "/opt/shared/data/transactions.csv"
 
 PROCESSED_DF_COLUMNS = ["cc_num", "trans_num", "trans_time", "category", "merchant", "amt", "merch_lat", "merch_long", "distance", "age", "is_fraud"]
 
@@ -16,7 +16,7 @@ KAFKA_TOPIC = 'transactions'
 PREPROCESSOR_PATH = 'spark_jobs/ml_training/artefacts/preprocessing'
 RANDOM_FOREST_MODEL_PATH = 'spark_jobs/ml_training/artefacts/randomforest'
 
-MLFLOW_TRACKING_URI = 'http:mlflow:5005'
+MLFLOW_TRACKING_URI = 'http://mlflow:5005'
 MLFLOW_EXPERIMENT_NAME = 'fraud-detection'
 
 SPARK_MASTER = 'spark://spark-master:7077'
