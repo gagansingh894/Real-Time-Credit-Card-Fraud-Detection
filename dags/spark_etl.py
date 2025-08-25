7ab453849d017ec647c1c319c86a46261e36a055
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2025-08-25 16:35:13 +0100

    fix: dag setup

--- dags/spark_etl.py
+++ dags/spark_etl.py
@@ -3,16 +3,8 @@ from datetime import datetime
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
-default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    'retries': 2,
-}
-
 with DAG(
     dag_id="spark_etl",
-    default_args=default_args,
-    start_date=datetime(2025, 8, 24),
     schedule_interval=None,
     catchup=False,
 ) as dag:
@@ -22,10 +14,6 @@ with DAG(
         name="spark_cassandra_etl",
         application="/opt/bitnami/spark/spark_jobs/etl/main.py",
         verbose=True,
-        conf={
-            'spark.master': 'spark://spark-master:7077',
-            'spark.submit.deployMode': 'cluster'
-        },
     )
 
 
